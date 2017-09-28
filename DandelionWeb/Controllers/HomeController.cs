04d91f66c024c1203c74a1a5e43f2f67c67b36ab
david.li <qiangli1973@gmail.com>
2017-09-28 13:41:31 -0400

    1st

---
+++ DandelionWeb/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DandelionWeb.Controllers
+{
+    //[RoutePrefix("")]
+    public class HomeController : Controller
+    {
+        //[Route("")]
+        public ActionResult Index()
+        {
+            return View("NgApp");
+           // return View();
+        }
+        [Route("trip")]
+        [Route("location")]
+        [Route("person")]
+        public ActionResult AppBookmarkableRoutes()
+        {
+            return View("NgApp");
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
